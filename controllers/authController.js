a45a9cd285f7e1603c0fb958a1910551ebfdb2a7
capmahin <mahinulhuque1996@gmail.com>
2023-02-26 00:16:42 +0600

    add name, email,password,phone,address

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
+import userModel from '../models/userModel';
+
+
 export const registerController = async (req, res) =>{
     try {
-        
+        const {name, email, password, phone, address}= req.body
     } catch (error) {
         console.log(error)
         res.status(500).send({
