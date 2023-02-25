d27ae9f07242f658cbdec13a2850586a76d9bf48
capmahin <mahinulhuque1996@gmail.com>
2023-02-26 00:19:31 +0600

    add name required

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,10 @@ import userModel from '../models/userModel';
 export const registerController = async (req, res) =>{
     try {
         const {name, email, password, phone, address}= req.body
+        //validations
+        if(!name){
+            return res.send({error:'Name is Required'})
+        }
     } catch (error) {
         console.log(error)
         res.status(500).send({
