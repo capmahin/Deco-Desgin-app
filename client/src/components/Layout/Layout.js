3b515958d1e8f120ea10392894839cc974e0795d
capmahin <mahinulhuque1996@gmail.com>
2023-02-27 01:15:17 +0600

    destracturing children

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from "./Header"
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div><h1>Layout</h1></div>
+    <div>
+        <Header/>
+        <main>
+        {children}
+        </main>
+    
+    <Footer/>
+    </div>
   )
 }
 
