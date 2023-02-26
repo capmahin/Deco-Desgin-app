acd20f630ff25621c5f7e06aad29857fa6a52732
capmahin <mahinulhuque1996@gmail.com>
2023-02-27 01:44:36 +0600

    add HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
-import Layout from './components/Layout/Layout'
+import {Routes,Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-    <Layout>
-    <h1>Deco-Design</h1>
-    </Layout>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+    </Routes>
      
     </>
   );
