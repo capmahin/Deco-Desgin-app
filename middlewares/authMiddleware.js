54b047f90e970395f8c6c2ee5dc5d672d6358afc
capmahin <mahinulhuque1996@gmail.com>
2023-02-27 00:21:48 +0600

    add userModel for admin user

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import JWT from 'jsonwebtoken'
+import userModel from "../models/userModel.js";
 
 //Protected Routes token base
 export const requireSignIn = async (req,res,next)=>{
@@ -11,9 +12,17 @@ export const requireSignIn = async (req,res,next)=>{
 };
 
 //admin access
-export const isAdmin = (req,res,next)=>{
+export const isAdmin = async (req,res,next)=>{
     try {
-        
+        const user = await userModel.findById(req.user._id)
+        if(user.role !== 1){
+            return res.status(401).send({
+                success:false,
+                message:'UnAuthorized Access'
+            })
+        }else{
+            
+        }
     } catch (error) {
         console.log(error)
     }
