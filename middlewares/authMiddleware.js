dc94876b95fa274c4870eeab8175e0ddb29a7c10
capmahin <mahinulhuque1996@gmail.com>
2023-02-26 14:10:59 +0600

    and try catch

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,5 +2,9 @@ import JWT from 'jsonwebtoken'
 
 //Protected Routes token base
 export const requireSignIn = async (req,res,next)=>{
-    const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+    try {
+        const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+    } catch (error) {
+        console.log(error)
+    }
 }
