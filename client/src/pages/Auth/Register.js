f1d608633506a049b65dec3a91c012d330fc251e
capmahin <mahinulhuque1996@gmail.com>
2023-02-27 16:43:17 +0600

    add required in all input field

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -18,7 +18,8 @@ const Register = () => {
     <input type="text" 
     value={name}
     onChange={(e)=>setName(e.target.value)}
-    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Name" />
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Name" 
+    required/>
    
   </div>
   <div className="mb-3">
@@ -26,7 +27,7 @@ const Register = () => {
     <input type="email"
     value={email}
     onChange={(e)=>setEmail(e.target.value)}
-    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Email"   />
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Email"  required />
    
   </div>
   <div className="mb-3">
@@ -34,14 +35,14 @@ const Register = () => {
     <input type="password"
     value={password} 
     onChange={(e)=>setPassword(e.target.value)}
-    className="form-control" id="exampleInputPassword1" placeholder="Enter Your password"  />
+    className="form-control" id="exampleInputPassword1" placeholder="Enter Your password" required />
   </div>
   <div className="mb-3">
     
     <input type="text" 
     value={phone}
     onChange={(e)=>setPhone(e.target.value)}
-    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Phone Number"  />
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Phone Number" required />
    
   </div>
   <div className="mb-3">
@@ -49,7 +50,7 @@ const Register = () => {
     <input type="text" 
     value={address}
     onChange={(e)=>setAddress(e.target.value)}
-    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Address"  />
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Address" required />
    
   </div>
   
