9986de0a86bf85bf5f4b2af781844d8647dee23b
capmahin <mahinulhuque1996@gmail.com>
2023-02-28 00:24:34 +0600

    add all things

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -7,17 +7,16 @@ import toast from 'react-hot-toast';
 import "../../styles/AuthStyle.css"
 
 const Login = () => {
-    const [name, setName] = useState("")
+   
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [phone, setPhone] = useState("")
-  const [address, setAddress] = useState("")
+ 
   const navigate = useNavigate();
 
   const handleSubmit = async(e)=>{
     e.preventDefault();
     try {
-       const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`,{name,email,password,phone,address});
+       const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/register`,{email,password});
        if( res && res.data.success){
         toast.success(res.data.message)
         navigate('/login')
@@ -33,17 +32,9 @@ const Login = () => {
   return (
     <Layout title={"Login - Deco Design"}>
     <div className="form-container">
-      <h4 className="title">REGISTER FORM</h4>
+      <h4 className="title">LOGIN FORM</h4>
       <form onSubmit={handleSubmit}>
-<div className="mb-3">
-  
-  <input type="text" 
-  value={name}
-  onChange={(e)=>setName(e.target.value)}
-  className="form-control" id="exampleInputEmail1" placeholder="Enter Your Name" 
-  required/>
- 
-</div>
+
 <div className="mb-3">
   
   <input type="email"
@@ -59,24 +50,10 @@ const Login = () => {
   onChange={(e)=>setPassword(e.target.value)}
   className="form-control" id="exampleInputPassword1" placeholder="Enter Your password" required />
 </div>
-<div className="mb-3">
-  
-  <input type="text" 
-  value={phone}
-  onChange={(e)=>setPhone(e.target.value)}
-  className="form-control" id="exampleInputEmail1" placeholder="Enter Your Phone Number" required />
- 
-</div>
-<div className="mb-3">
 
-  <input type="text" 
-  value={address}
-  onChange={(e)=>setAddress(e.target.value)}
-  className="form-control" id="exampleInputEmail1" placeholder="Enter Your Address" required />
- 
-</div>
 
-<button type="submit" className="btn btn-primary">Submit</button>
+
+<button type="submit" className="btn btn-primary">Login</button>
 </form>
 
     </div>
