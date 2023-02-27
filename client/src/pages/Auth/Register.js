16d576302fabe05ee26e4fdb864d14adeb4ea864
capmahin <mahinulhuque1996@gmail.com>
2023-02-27 12:54:04 +0600

    Create register page

---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './../../components/Layout/Layout';
+
+const Register = () => {
+  return (
+    <Layout title={"Register - Deco Design"}>
+        <h1>Register Page</h1>
+    </Layout>
+  )
+}
+
+export default Register
