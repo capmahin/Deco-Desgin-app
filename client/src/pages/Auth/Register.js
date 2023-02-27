5f2acab02cc2293eca1852dc789293bbe3c3a2d7
capmahin <mahinulhuque1996@gmail.com>
2023-02-27 12:58:52 +0600

    add css class for register page

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -4,7 +4,9 @@ import Layout from './../../components/Layout/Layout';
 const Register = () => {
   return (
     <Layout title={"Register - Deco Design"}>
+      <div className="register">
         <h1>Register Page</h1>
+      </div>
     </Layout>
   )
 }
