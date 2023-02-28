9937dc090601d349cec2523b207cdb78a203bb2f
capmahin <mahinulhuque1996@gmail.com>
2023-02-28 13:38:42 +0600

    add authoraization

--- client/src/components/Layout/Routes/Private.js
+++ client/src/components/Layout/Routes/Private.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useAuth} from "../../../context/auth";
 import { Outlet } from "react-router-dom";
+import axios from "axios";
 
 export default function PrivateRoute(){
     const [ok,setOk] = useState(false)
@@ -8,6 +9,16 @@ export default function PrivateRoute(){
 
     useEffect(()=>{
 
+         const authCheck= async()=>{
+            const res = await axios.get('/api/v1/auth/user-auth',{
+                headers:{
+                    "Authorization":auth?.token
+                }
+            })
+            if(res.data.ok){
+                
+            }
+         }
     },[])
 
     return ok ?<Outlet/> : 'spinner'
