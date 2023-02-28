fdc60cf3f1eae8fc80bff1277bf7a7ab6f8b2926
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 00:15:42 +0600

    add isadmin

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -25,5 +25,10 @@ router.get('/test',requireSignIn, isAdmin, testController);
 router.get('/user-auth', requireSignIn, (req,res)=>{
     res.status(200).send({ok:true});
 });
+//protected route auth
+router.get('/admin-auth', requireSignIn,isAdmin, (req,res)=>{
+    res.status(200).send({ok:true});
+});
+
 
 export default router
