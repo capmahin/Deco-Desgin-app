85342f48ba0d0964784df9d3b197aa31e09f1a01
capmahin <mahinulhuque1996@gmail.com>
2023-02-28 14:04:50 +0600

    add interval

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useNavigate } from "react-router-dom";
 
 const Spinner = () => {
+    const [count, setCount] = useState(5)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setCount((prevValue)=> --prevValue)
+        },1000)
+    },[])
   return (
     <>
-    <div className="d-flex justify-content-center align-items-center" style={{height:'80vh'}}>
+    <div className="d-flex justify-content-center align-items-center" style={{height:'100vh'}}>
   <div className="spinner-border" role="status">
     <span className="visually-hidden">Loading...</span>
   </div>
