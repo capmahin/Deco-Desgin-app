432e1a622dfde3c2d00d4a752c57d231e0595a26
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 00:17:55 +0600

    add Adminroute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -21,11 +21,11 @@ router.post('/forgot-password', forgotPasswordController)
 //test routes
 router.get('/test',requireSignIn, isAdmin, testController);
 
-//protected route auth
+//protected User route auth
 router.get('/user-auth', requireSignIn, (req,res)=>{
     res.status(200).send({ok:true});
 });
-//protected route auth
+//protected Admin route auth
 router.get('/admin-auth', requireSignIn,isAdmin, (req,res)=>{
     res.status(200).send({ok:true});
 });
