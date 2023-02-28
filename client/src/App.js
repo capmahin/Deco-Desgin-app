01511be72f3d3224625cb624c249a9a5acb99854
capmahin <mahinulhuque1996@gmail.com>
2023-02-28 13:59:12 +0600

    add align-items-center in spinner

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,16 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/Auth/Login';
 import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Layout/Routes/Private";
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/dashboard" element={<PrivateRoute/>}>
+      <Route path="" element={<Dashboard/>}/>
+      </Route>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/about" element={<About/>}/>
--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Spinner = () => {
   return (
     <>
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center align-items-center" style={{height:'80vh'}}>
   <div className="spinner-border" role="status">
     <span className="visually-hidden">Loading...</span>
   </div>
