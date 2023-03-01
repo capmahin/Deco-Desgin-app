06ed41f7f39f4def96a3c15cbb74f1f3d1ecb1b7
capmahin <mahinulhuque1996@gmail.com>
2023-03-02 01:37:16 +0600

    add

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -41,7 +41,12 @@ export const updateCategoryController =async (req,res) =>{
         const {id} = req.params
         const category = await categoryModel.findByIdAndUpdate(id,{
             name, slug:slugify(name)
-        },{new:true})
+        },{new:true});
+        res.status(200).send({
+            success:true,
+            message:"Category Updated Successfully",
+            category
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send({
