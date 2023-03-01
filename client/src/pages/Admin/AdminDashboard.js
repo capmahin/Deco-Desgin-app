f6f07aa1bff2c7c5a608769459a983c915c95db1
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 12:15:22 +0600

    add useAuth

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import AdminMenu from "../../components/Layout/AdminMenu"
 import Layout from "../../components/Layout/Layout"
-
+import { useAuth } from "../../context/auth"
 const AdminDashboard = () => {
+  const [auth] = useAuth()
   return (
-    <Layout><div className="container-fluid">
+    <Layout><div className="container-fluid m-3 p-3">
       <div className="row">
         <div className="col-md-3">
           <AdminMenu/>
         </div>
         <div className="col-md-9">
-         Content
+         <div className="card">
+          <h1>{auth?.user?.name}</h1>
+         </div>
         </div>
       </div>
       </div></Layout>
