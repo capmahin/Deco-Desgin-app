56493d5f491dafea2d71e333b1dae9b8578c45d8
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 13:38:33 +0600

    working on Dashboard

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import UserMenu from "../../components/Layout/UserMenu";
 import Layout from './../../components/Layout/Layout';
 
 const Dashboard = () => {
   return (
     <Layout title={'User Dashboard'}>
-        <h1>Dashboard</h1>
+       <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu/>
+          </div>
+          <div className="col-md-9"></div>
+        </div>
+       </div>
     </Layout>
   )
 }
