61ed9a64f239ee15f1b11ba30b59a75a9bc4b96d
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 18:47:10 +0600

    add middlerWare

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,9 @@
 import  express  from "express";
+import { isAdmin, requireSignIn } from './../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
 //routers
-router.post()
+router.post('create-category', requireSignIn, isAdmin)
 
 export default router
