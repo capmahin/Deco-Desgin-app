a0e40e6b96fb0054cdb5f1a3eca7fa691968ffdd
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 13:25:11 +0600

    solve name

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -4,7 +4,7 @@ import Layout from "../../components/Layout/Layout"
 
 const CreateCategory = () => {
   return (
-    <Layout title={'Dashboard-Category'}>
+    <Layout title={'Dashboard-Create-Category'}>
         <div className="container-fluid m-3 p-3">
         <div className="row">
     <div className="col-md-3">
