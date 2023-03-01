90e32236d9f49c8152229eb4966dd9bbc4737e92
capmahin <mahinulhuque1996@gmail.com>
2023-03-02 02:12:08 +0600

    add deleteCategoryController

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -17,4 +17,7 @@ router.get('/get-category', categoryController);
 //single category
 router.get('/single-category/:slug', singleCategoryController);
 
+//delete category
+router.delete('/delete-category/:id', requireSignIn, isAdmin, deleteCategoryController);
+
 export default router
