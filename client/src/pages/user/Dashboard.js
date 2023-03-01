91fcd27deb2fa163d936c38b4d0c839b108474e4
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 13:41:33 +0600

    add useAuth

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import UserMenu from "../../components/Layout/UserMenu";
 import Layout from './../../components/Layout/Layout';
-
+import { useAuth } from "../../context/auth";
 const Dashboard = () => {
+  const [auth] = useAuth()
   return (
     <Layout title={'User Dashboard'}>
        <div className="container-fluid m-3 p-3">
@@ -10,7 +11,13 @@ const Dashboard = () => {
           <div className="col-md-3">
             <UserMenu/>
           </div>
-          <div className="col-md-9"></div>
+          <div className="col-md-9">
+            <div className="card w-75 p-3">
+               <h3>{auth?.user?.name}</h3>
+               <h3>{auth?.user?.email}</h3>
+               <h3>{auth?.user?.address}</h3>
+            </div>
+          </div>
         </div>
        </div>
     </Layout>
