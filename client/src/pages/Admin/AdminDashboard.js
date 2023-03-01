f659a4c9f282566311cd03580aecb8934f212885
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 12:12:33 +0600

    add cols in AdminDashBoard

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import AdminMenu from "../../components/Layout/AdminMenu"
 import Layout from "../../components/Layout/Layout"
 
 const AdminDashboard = () => {
   return (
-    <Layout><h1>AdminDashboard</h1></Layout>
+    <Layout><div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3">
+          <AdminMenu/>
+        </div>
+        <div className="col-md-9">
+         Content
+        </div>
+      </div>
+      </div></Layout>
   )
 }
 
