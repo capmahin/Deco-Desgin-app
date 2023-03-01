7d0d0d882f660e67320e2266c7170a33620df84c
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 13:20:22 +0600

    add dive style in 3 files

--- client/src/pages/Admin/Users.js
+++ client/src/pages/Admin/Users.js
@@ -5,6 +5,7 @@ import Layout from "../../components/Layout/Layout"
 const Users = () => {
   return (
     <Layout>
+        <div className="container-fluid m-3 p-3">
         <div className="row">
             <div className="col-md-3">
                 <AdminMenu/>
@@ -12,6 +13,7 @@ const Users = () => {
             <div className="col-md-9">
             <h1>Users</h1>
             </div>
+        </div>
         </div>
         </Layout>
   )
