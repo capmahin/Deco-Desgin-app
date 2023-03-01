58d8c2919e595cea6e9a00867e58735455868f95
capmahin <mahinulhuque1996@gmail.com>
2023-03-02 02:02:20 +0600

    add error

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -80,4 +80,15 @@ try {
 
 export const singleCategoryController = async (req,res) =>{
 
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            error,
+            message:'Error While getting single Category'
+        })
+        
+    }
 };
