4c2e6443c6a283e4355fb509fbe49f0df451ff9d
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 13:27:41 +0600

    change path

--- client/src/components/Layout/UserMenu.js
+++ client/src/components/Layout/UserMenu.js
@@ -6,10 +6,10 @@ const UserMenu = () => {
     <div>
          <div className="text-center">
     <div className="list-group">
- <h1>Admin Panel</h1>
-  <NavLink to="/dashboard/admin/create-category" className="list-group-item list-group-item-action">Create Category</NavLink>
-  <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action">Create Product</NavLink>
-  <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action">Users</NavLink>
+ <h1>Dashboard</h1>
+  <NavLink to="/dashboard/user/create-category" className="list-group-item list-group-item-action">Create Category</NavLink>
+  <NavLink to="/dashboard/user/create-product" className="list-group-item list-group-item-action">Create Product</NavLink>
+  <NavLink to="/dashboard/user/users" className="list-group-item list-group-item-action">Users</NavLink>
   
 </div>
     </div>
