b382852f474df7c3edccfdcffbdb3a38968407ab
capmahin <mahinulhuque1996@gmail.com>
2023-03-02 00:42:51 +0600

    add if existing Catgory exis

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -9,6 +9,12 @@ export const createCategoryController = async (req,res) =>{
             return res.status(401).send({message:'Name is required'})
         }
         const existingCategory = await categoryModel.findOne({name})
+        if(existingCategory){
+            return res.status(200).send({
+                success:true,
+                message:'Category Already Exisits'
+            })
+        }
         
     } catch (error) {
         console.log(error)
