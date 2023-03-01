e1f4d3ba44b1f06eebbbce3babf084a91b54bff4
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 12:09:45 +0600

    add to property

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -7,9 +7,9 @@ const AdminMenu = () => {
     <div className="text-center">
     <div className="list-group">
  <h1>Admin Panel</h1>
-  <NavLink to="" className="list-group-item list-group-item-action">Create Category</NavLink>
-  <NavLink to="" className="list-group-item list-group-item-action">Create Product</NavLink>
-  <NavLink to="" className="list-group-item list-group-item-action">Users</NavLink>
+  <NavLink to="/dashboard/admin/create-category" className="list-group-item list-group-item-action">Create Category</NavLink>
+  <NavLink to="dashboard/admin/create-product" className="list-group-item list-group-item-action">Create Product</NavLink>
+  <NavLink to="dashboard/admin/users" className="list-group-item list-group-item-action">Users</NavLink>
   
 </div>
     </div>
