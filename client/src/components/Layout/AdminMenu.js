fe4c801665beaf163198696a1ac20f60aa75f8de
capmahin <mahinulhuque1996@gmail.com>
2023-03-01 12:07:46 +0600

    add

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -4,13 +4,16 @@ import { NavLink } from "react-router-dom"
 const AdminMenu = () => {
   return (
     <>
-  <div className="list-group">
- 
+    <div className="text-center">
+    <div className="list-group">
+ <h1>Admin Panel</h1>
   <NavLink to="" className="list-group-item list-group-item-action">Create Category</NavLink>
   <NavLink to="" className="list-group-item list-group-item-action">Create Product</NavLink>
   <NavLink to="" className="list-group-item list-group-item-action">Users</NavLink>
   
 </div>
+    </div>
+  
 
 
     </>
