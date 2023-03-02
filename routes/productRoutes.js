fe90d2538d3b3b0dd070d81bb56388fe6095a213
capmahin <mahinulhuque1996@gmail.com>
2023-03-03 00:21:00 +0600

    add productController in

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from './../middlewares/authMiddleware.js';
-import { createProductController } from './../controllers/productController.js';
+import { createProductController, getProductController } from './../controllers/productController.js';
 import formidable from 'express-formidable'
 
 const router = express.Router()
@@ -9,5 +9,5 @@ const router = express.Router()
 router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController);
 
 //get products
-router.get('/get-product', getProductController)
+router.get('/get-product', getProductController);
 export default router;
