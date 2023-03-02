1d58b993d8bd8534ce2056f3192d22dd3ab3b7c4
capmahin <mahinulhuque1996@gmail.com>
2023-03-02 13:48:42 +0600

    add switch

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,6 +5,23 @@ export const createProductController = async (req,res)=>{
     try {
         const {name, slug, description,price,category,quantity,shipping} = req.fields
         const {photo} = req.files
+        //validation
+        switch(true){
+            case !name:
+                return res.status(500).send({error:'Name is Required'})
+            case !slug:
+                return res.status(500).send({error:'Slug is Required'})
+            case !description:
+                return res.status(500).send({error:'Description is Required'})
+            case !price:
+                return res.status(500).send({error:'Price is Required'})
+            case !category:
+                return res.status(500).send({error:'Category is Required'})
+            case !quantity:
+                return res.status(500).send({error:'Quantity is Required'})
+            case !shipping:
+                return res.status(500).send({error:'Shipping is Required'})
+        }
         const products = await productModel
     } catch (error) {
         console.log(error)
