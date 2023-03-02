80dc92f5b95948a2cc804ea544efb20526919031
capmahin <mahinulhuque1996@gmail.com>
2023-03-03 00:28:41 +0600

    add res.status(200)

--- controllers/productController.js
+++ controllers/productController.js
@@ -48,6 +48,11 @@ export const createProductController = async (req,res)=>{
 export const getProductController = async (req,res)=>{
     try {
         const products = await productModel.find({}).select("-photo").limit(12).sort({createdAt:-1})
+        res.status(200).send({
+            success:true,
+            message:'All Products',
+            products,
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send({
