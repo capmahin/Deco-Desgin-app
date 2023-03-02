f376f7f8be74e8e64f2722522a400e916d66df45
capmahin <mahinulhuque1996@gmail.com>
2023-03-02 13:38:07 +0600

    add req.field and req.file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,10 @@
 import productModel from "../models/productModel.js"
+import fs from 'fs'
 
 export const createProductController = async (req,res)=>{
     try {
+        const {name, slug, description,price,category,quantity,shipping} = req.fields
+        const {photo} = req.files
         const products = await productModel
     } catch (error) {
         console.log(error)
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,4 +7,4 @@ const router = express.Router()
 
 //routes
 router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController)
-export default router
+export default router;
