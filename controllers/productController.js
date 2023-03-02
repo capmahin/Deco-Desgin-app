dcd1a2872152f269f8a128d8b8a5b5db4fe9308a
capmahin <mahinulhuque1996@gmail.com>
2023-03-03 00:23:20 +0600

    add try catch

--- controllers/productController.js
+++ controllers/productController.js
@@ -46,5 +46,15 @@ export const createProductController = async (req,res)=>{
 //get all product
 
 export const getProductController = async (req,res)=>{
-    
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error in geting product',
+            error,
+        })
+        
+    }
 }
