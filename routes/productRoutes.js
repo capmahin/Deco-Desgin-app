8dea43cf4ad6127b24178fe8a203ab6247a0adc7
capmahin <mahinulhuque1996@gmail.com>
2023-03-02 13:21:19 +0600

    add middleware

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import { isAdmin, requireSignIn } from './../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
-
+//routes
+router.post('/create-product', requireSignIn, isAdmin)
 export default router
