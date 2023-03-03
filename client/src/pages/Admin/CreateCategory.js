871db9925b1820a1331992a998e35a7b9b4dc9ab
capmahin <mahinulhuque1996@gmail.com>
2023-03-03 20:09:46 +0600

    add table

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -18,7 +18,11 @@ const CreateCategory = () => {
       console.log(error)
       toast.error('Something went wrong in getting category')
     }
-  }
+  };
+
+  useEffect(()=>{
+    getAllCategory();
+  },[])
   return (
     <Layout title={'Dashboard-Create-Category'}>
         <div className="container-fluid m-3 p-3">
@@ -27,7 +31,28 @@ const CreateCategory = () => {
         <AdminMenu/>
     </div>
     <div className="col-md-9">
-    <h1>Create Category</h1>
+    <h1>Manage Category</h1>
+    <div>
+   <table className="table">
+  <thead>
+    <tr>
+  
+      <th scope="col">Name</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+    </div>
     </div>
 </div>
 </div>
