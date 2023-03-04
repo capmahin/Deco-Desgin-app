c212627152e7a9437a8cb287cbc8c32ad0747a09
capmahin <mahinulhuque1996@gmail.com>
2023-03-05 01:08:17 +0600

    add option

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -3,12 +3,15 @@ import AdminMenu from "../../components/Layout/AdminMenu"
 import Layout from "../../components/Layout/Layout"
 import toast from 'react-hot-toast';
 import axios from "axios";
+import {Select} from 'antd'
+const {Option} = Select
 
 const CreateProduct = () => {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
   const [quantity, setQuantity] = useState("");
   const [shipping, setShipping] = useState("");
   
@@ -16,8 +19,8 @@ const CreateProduct = () => {
   const getAllCategory = async () =>{
     try {
       const {data} = await axios.get('/api/v1/category/get-category')
-      if(data.success){
-        setCategories(data.category)
+      if(data?.success){
+        setCategories(data?.category)
       }
     } catch (error) {
       console.log(error)
@@ -37,6 +40,19 @@ const CreateProduct = () => {
     </div>
     <div className="col-md-9">
     <h1>Create Products</h1>
+    <div className="m-1 w-75">
+      <Select bordered={false} placeholder ='Select a category' size="large" showSearch className="form-select mb-3"
+      onChange={(value)=>{setCategory(value)}}
+      >
+ {
+  categories?.map(c => (
+    <Option key={c._id}>
+      {c.name}
+    </Option>
+  ))
+ }
+      </Select>
+    </div>
     </div>
 </div>
 </div>
