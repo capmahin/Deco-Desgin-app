b01226042f05c22a50150974aa1c496a523feae7
capmahin <mahinulhuque1996@gmail.com>
2023-03-05 01:29:29 +0600

    add preview

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -55,13 +55,21 @@ const CreateProduct = () => {
       </Select>
 
       <div className="mb-3">
-        <label htmlFor="upload images" className="btn btn-outline-secondary">
-          {photo && photo.name} : "Upload Photo"
+        <label  className="btn btn-outline-secondary col-md-12">
+          {photo ? photo.name : "Upload Photo"} Upload Photo
           <input type="file" name="photo" accept="image/*" 
           onChange={(e)=> setPhoto(e.target.files[0])}
           hidden
           />
         </label>
+      </div>
+      <div className="mb-3">
+        {photo && (
+          <div className="text-center">
+            <img src={URL.createObjectURL(photo)} alt="product-photo" height={'200px'}
+            className="img img-responsive" />
+          </div>
+        )}
       </div>
     </div>
     </div>
