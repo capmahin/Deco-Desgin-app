ded8c6e8d4db751cd50ea1dbc57642544fbb92d2
capmahin <mahinulhuque1996@gmail.com>
2023-03-05 15:16:15 +0600

    add

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -33,12 +33,12 @@ const Products = () => {
             <div className="col-md-9">
                 <h1 className="text-center">All Products List</h1>
                 {products?.map(p=>(
-                    <div className="card" style={{width: '18rem'}}>
-  <img src="..." className="card-img-top" alt="..." />
+                    <div className="card" style={{width: '18rem'}} key={p._id}>
+  <img src={p.photo} className="card-img-top" alt={p.name} />
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{p.name}</h5>
+    <p className="card-text">{p.description}</p>
+    
   </div>
 </div>
 
