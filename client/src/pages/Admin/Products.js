783c7e8a680e52e8eded5afcd59e339e64fb2582
capmahin <mahinulhuque1996@gmail.com>
2023-03-05 14:52:12 +0600

    add Products.js in Admin

---
+++ client/src/pages/Admin/Products.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from './../../components/Layout/Layout';
+
+const Products = () => {
+  return (
+    <Layout>Products</Layout>
+  )
+}
+
+export default Products
