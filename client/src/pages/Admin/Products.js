38517fedf881d731bf11aacd918169e98d004db0
capmahin <mahinulhuque1996@gmail.com>
2023-03-05 14:55:39 +0600

    add layout

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import AdminMenu from "../../components/Layout/AdminMenu"
+import Layout from './../../components/Layout/Layout';
 
 
 const Products = () => {
   return (
-    <div>
+    <Layout>
         <div className="row">
             <div className="col-md-3">
             <AdminMenu/>
             </div>
             <div className="col-md-9">
-                
+                <h1 className="text-center">All Products List</h1>
             </div>
         </div>
-    </div>
+    </Layout>
   )
 }
 
