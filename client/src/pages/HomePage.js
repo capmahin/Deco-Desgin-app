eccd074ca28c742f7b029bbd73967e5f0f8d178b
capmahin <mahinulhuque1996@gmail.com>
2023-03-07 13:30:26 +0600

    add setProduct

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -12,6 +12,7 @@ const HomePage = () => {
   const getAllProducts = async()=>{
     try {
       const {data} = await axios.get('/api/v1/product/get-product')
+      setProducts(data.products);
     } catch (error) {
       console.log(error)
     }
