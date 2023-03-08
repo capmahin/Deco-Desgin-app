bcd6462b9e526000c6ed94502c4ecf2518cd8358
capmahin <mahinulhuque1996@gmail.com>
2023-03-08 12:59:33 +0600

    add productListController

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from './../middlewares/authMiddleware.js';
-import { createProductController, deleteProductController, getProductController, getSingleProductController, productCountController, productFiltersController, productPhotoController, updateProductController } from './../controllers/productController.js';
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productCountController, productFiltersController, productListController, productPhotoController, updateProductController } from './../controllers/productController.js';
 import formidable from 'express-formidable'
 
 const router = express.Router()
@@ -31,6 +31,6 @@ router.post('/product-filters', productFiltersController);
 router.get('/product-count', productCountController);
 
 //product per page
-router.get('/product-list/:page', productListController)
+router.get('/product-list/:page', productListController);
 
 export default router;
