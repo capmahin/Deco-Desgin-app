ac8e7df709d2e74ec4f55811aca81f88b4a384c0
capmahin <mahinulhuque1996@gmail.com>
2023-03-09 01:20:17 +0600

    pass result in res.json

--- controllers/productController.js
+++ controllers/productController.js
@@ -237,12 +237,13 @@ export const productListController = async (req,res) =>{
 export const searchProductController =  async (req,res)=>{
     try {
         const {keyword} = req.params
-        const result = await productModel.find({
+        const results = await productModel.find({
             $or:[
                 {name:{$regex: keyword, $options:"i"}},
                 {description:{$regex: keyword, $options:"i"}}
             ]
         }).select("-photo")
+        res.json(results)
     } catch (error) {
         console.log(error)
         res.status(400).send({
