a25b9e11d7761252f351506eba108a7ab6efd3cf
capmahin <mahinulhuque1996@gmail.com>
2023-03-08 12:58:35 +0600

    pass products

--- controllers/productController.js
+++ controllers/productController.js
@@ -218,6 +218,10 @@ export const productListController = async (req,res) =>{
         const perPage = 6
         const page = req.params.page ? req.params.page : 1
         const products = await productModel.find({}).select("-photo").skip((page-1) * perPage).limit(perPage).sort({createdAt: -1});
+        res.status(200).send({
+            success:true,
+            products,
+        })
     } catch (error) {
         console.log(error)
         res.status(400).send({
