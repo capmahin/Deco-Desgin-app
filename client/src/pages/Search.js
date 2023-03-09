65427419a420104e75ceb714db518ea29166956d
capmahin <mahinulhuque1996@gmail.com>
2023-03-09 12:37:53 +0600

    add useSearch hook

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Layout from './../components/Layout/Layout';
-
+import { useSearch } from "../context/search";
 const Search = () => {
   return (
     <Layout title={'Search-result'}>
-        Search
+        <div className="container">
+            <div className="text-center">
+                <h1>Search Results</h1>
+                <h5>{}</h5>
+            </div>
+        </div>
         </Layout>
 
   )
