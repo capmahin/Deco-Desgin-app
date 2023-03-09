7337f925bb9f04f28e92f9453c43cd0df1f44148
capmahin <mahinulhuque1996@gmail.com>
2023-03-09 14:31:44 +0600

    add more

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -17,16 +17,18 @@ const ProductDetails = () => {
     const getProduct = async () =>{
         try {
             const {data} = await axios.get(`/api/v1/product/get-product/${params.slug}`);
-            setProduct(data?.product)
+            setProduct(data?.product);
+            getSimilarProduct(data?.product._id, data?.product.category._id)
         } catch (error) {
             console.log(error)
         }
     };
 
     //get similar product
-    const getSmilarProduct = async(pid,cid)=>{
+    const getSimilarProduct = async(pid,cid)=>{
         try {
             const {data} = await axios.get(`/api/v1/product/related-product/${pid}/${cid}`)
+            setRelatedProducts(data?.products)
         } catch (error) {
             console.log(error)
         }
@@ -46,12 +48,13 @@ const ProductDetails = () => {
             <h5>Name : {product.name}</h5>
             <h5>Description : {product.description}</h5>
             <h5>Price : {product.price}</h5>
-            <h6>Category: {product.category.name}</h6>
+            {/* <h6>Category: {product.category.name}</h6> */}
             <button class="btn btn-secondary ms-1">ADD TO CART</button>
         </div>
        </div>
        <div className="row">
-        similar products
+        <h1>Similar Product</h1>
+        {JSON.stringify(relatedProducts, null, 4)}
        </div>
     </Layout>
   )
