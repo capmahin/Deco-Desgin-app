6af4bf6812f811ef52a19405f1be8ac57fa9d675
capmahin <mahinulhuque1996@gmail.com>
2023-03-09 13:12:16 +0600

    add  getProduct async

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Layout from './../components/Layout/Layout';
 
 const ProductDetails = () => {
+    //getProduct
+
+    const getProduct = async () =>{
+        
+    }
   return (
     <Layout title={'Product-details'}>
         <h1>Product Details</h1>
