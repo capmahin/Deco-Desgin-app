5b1028a4ff76f61f8ba8572e29b3405f8432a412
capmahin <mahinulhuque1996@gmail.com>
2023-03-09 13:16:21 +0600

    add axios for network

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,11 +1,18 @@
 import React,{useState,useEffect} from 'react'
 import Layout from './../components/Layout/Layout';
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
+    const params = useParams();
     //getProduct
 
     const getProduct = async () =>{
-        
+        try {
+            const {data} = await axios.get(`/api/v1/product/get-product/${params.slug}`)
+        } catch (error) {
+            console.log(error)
+        }
     }
   return (
     <Layout title={'Product-details'}>
