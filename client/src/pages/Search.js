04778c023f17d699ccb8dc47ccf0a4e227898d0e
capmahin <mahinulhuque1996@gmail.com>
2023-03-09 12:40:37 +0600

    add condition

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Layout from './../components/Layout/Layout';
 import { useSearch } from "../context/search";
 const Search = () => {
+    const [values, setValues] = useSearch()
   return (
     <Layout title={'Search-result'}>
         <div className="container">
             <div className="text-center">
                 <h1>Search Results</h1>
-                <h5>{}</h5>
+                <h5>{values?.results.length < 1 ? 'No Product found' : 
+                `Found ${values?.results.length}`
+                }</h5>
             </div>
         </div>
         </Layout>
