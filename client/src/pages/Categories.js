d0618681f725002446ad548f077d873b530a7c87
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 02:07:22 +0600

    add map

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react';
+import { Link } from "react-router-dom";
 import useCategory from "../hooks/useCategory";
 import Layout from './../components/Layout/Layout';
 
@@ -6,7 +7,16 @@ const Categories = () => {
     const categories = useCategory();
   return (
     <Layout title={'All Categories'}>
-        <h1>All Categories</h1>
+        <div className="container">
+            <div className="row">
+                {categories.map(c => (
+                   <div className="col-md-6">
+                   <button><Link to="/">{c.name}</Link></button>
+               </div>
+                ))}
+                
+            </div>
+        </div>
     </Layout>
   )
 }
