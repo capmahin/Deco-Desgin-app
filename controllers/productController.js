7576a01ba1b7f2a68a4142ebdda2783fe9d284c1
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 02:28:43 +0600

    add category and products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 import productModel from "../models/productModel.js"
+import categoryModel from "../models/categoryModel.js"
 import fs from 'fs'
 import  slugify  from 'slugify';
 
@@ -281,7 +282,13 @@ export const relatedProductController = async (req,res) =>{
 
 export const productCategoryController = async(req,res)=>{
     try {
-        
+        const category = await categoryModel.findOne({slug:req.params.slug});
+        const products = await productModel.find({category}).populate('category');
+        res.status(200).send({
+            success:true,
+            category,
+            products,
+        })
     } catch (error) {
         console.log(error)
         res.status(400).send({
