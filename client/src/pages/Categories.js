d71a6003366c92433408b0f3261d87d7e0c93c5e
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 02:01:40 +0600

    import categories page in app.js

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -3,7 +3,9 @@ import Layout from './../components/Layout/Layout';
 
 const Categories = () => {
   return (
-    <Layout title={'All Categories'}>Categories</Layout>
+    <Layout title={'All Categories'}>
+        <h1>All Categories</h1>
+    </Layout>
   )
 }
 
