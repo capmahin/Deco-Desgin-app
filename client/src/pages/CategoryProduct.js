57a38805d4833c3bdefa0ff15b07323208058e98
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 02:37:11 +0600

    add category state

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -5,10 +5,12 @@ import axios from "axios";
 const CategoryProduct = () => {
     const  params = useParams();
     const [products, setProducts] = useState([]);
+    const [category, setCategory] = useState([]);
     const getProductsByCat = async () =>{
         try {
             const {data} = await axios.get(`/api/v1/product/product-category/${params.slug}`);
             setProducts(data?.products);
+            setCategory(data?.category);
         } catch (error) {
             console.log(error)
         }
