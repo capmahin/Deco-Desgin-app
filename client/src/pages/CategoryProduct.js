b127df926328460d7315c8f0edc7c651f703a336
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 02:15:22 +0600

    add categoryProduct

---
+++ client/src/pages/CategoryProduct.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './../components/Layout/Layout';
+
+const CategoryProduct = () => {
+  return (
+    <Layout>
+        <div className="container">
+           <h1>Cat Name</h1>
+        </div>
+    </Layout>
+  )
+}
+
+export default CategoryProduct
