9016b22996bbc2ca4126f2347cdc543ba654b3b0
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 02:48:01 +0600

    add all products

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -1,11 +1,16 @@
 import React,{useState,useEffect} from 'react'
 import Layout from './../components/Layout/Layout';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 const CategoryProduct = () => {
     const  params = useParams();
+    const navigate = useNavigate();
     const [products, setProducts] = useState([]);
     const [category, setCategory] = useState([]);
+
+    useEffect(()=>{
+        if(params?.slug) getProductsByCat()
+    },[params?.slug])
     const getProductsByCat = async () =>{
         try {
             const {data} = await axios.get(`/api/v1/product/product-category/${params.slug}`);
@@ -17,8 +22,49 @@ const CategoryProduct = () => {
     }
   return (
     <Layout>
-        <div className="container">
-           <h1>Cat Name</h1>
+        <div className="container mt-3">
+           <h3 className="text-center">Category - {category?.name}</h3>
+           <h4 className="text-center">{products?.length} result</h4>
+           <div className="row">
+           <div className="col-md-9 offset-1">
+       
+       <h1 className="text-center">All Products</h1>
+       <div className="d-flex flex-wrap">
+         {products?.map(p=>(      
+            <div className="card m-2" style={{width: '18rem'}} >
+             <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} height="200" />
+             <div className="card-body">
+             <h5 className="card-title">{p.name}</h5>
+             <p className="card-text">{p.description.substring(0, 60)}...</p>
+             <p className="card-text">$ {p.price}</p>
+             <button class="btn btn-primary ms-1" 
+             
+             onClick={()=> navigate(`/product/${p.slug}`) }>More Details</button>
+             <button class="btn btn-secondary ms-1">ADD TO CART</button>
+            </div>
+           </div>
+                  
+               
+               ))}
+         </div>
+         {/* <div className="m-2 p-3">
+           {
+             products && products.length <total && (
+               <button className="btn btn-warning"
+               onClick={(e)=>{
+                 e.preventDefault();
+                 setPage(page + 1);
+               }}
+               >
+                 {
+                   loading ? "Loading ..." : "Loadmore"
+                 }
+               </button>
+             )
+           }
+         </div> */}
+         </div> 
+           </div>
         </div>
     </Layout>
   )
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -144,7 +144,7 @@ const loadMore = async() =>{
          <button className="btn btn-danger" onClick={()=> window.location.reload()}>RESET FILTERS</button>
        </div>
         </div> 
-       <div className="col-md-9">
+       <div className="col-md-9 offset-1">
        
         <h1 className="text-center">All Products</h1>
         <div className="d-flex flex-wrap">
