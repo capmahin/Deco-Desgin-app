53cb65142b415acd03eca62beb7683ef1b6792fd
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 02:03:34 +0600

    call categories

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import useCategory from "../hooks/useCategory";
 import Layout from './../components/Layout/Layout';
 
 const Categories = () => {
+    const categories = useCategory();
   return (
     <Layout title={'All Categories'}>
         <h1>All Categories</h1>
