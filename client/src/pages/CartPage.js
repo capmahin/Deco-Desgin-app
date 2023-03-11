2ba83245af6984946337aaac7925ed69f8a123b4
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 16:15:30 +0600

    add all

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -22,13 +22,13 @@ const CartPage = () => {
                 </h4>
               </div>
             </div>
-            <div className="row">
+            <div className="row ">
               <div className="col-md-9">
                 {
                   cart?.map( p => (
-                    <div className="row">
+                    <div className="row mb-2 card flex-row">
                       <div className="col-md-4">
-                      <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} height="200" />
+                      <img src={`/api/v1/product/product-photo/${p._id}`} className="card-img-top" alt={p.name} width="100px" height= {"100px"} />
                       </div>
                       <div className="col-md-8">
                         details
