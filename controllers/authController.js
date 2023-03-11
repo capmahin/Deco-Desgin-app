46f1b4008b9e59fd9d3272a6ddfac9e519553cd8
capmahin <mahinulhuque1996@gmail.com>
2023-03-12 01:14:27 +0600

    add

--- controllers/authController.js
+++ controllers/authController.js
@@ -175,6 +175,11 @@ export const updateProfileController = async(req,res)=>{
             phone:phone || user.phone,
             address:address || user.address
         },{new:true})
+        res.status(200).send({
+            success:true,
+            message:'Profile Updated Successfully',
+            updatedUser
+        })
     } catch (error) {
         console.log(error)
         res.status(400).send({
