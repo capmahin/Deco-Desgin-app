4a6434ca3313aa1da4bfe7244f9190d732d22753
capmahin <mahinulhuque1996@gmail.com>
2023-03-12 00:42:44 +0600

    add update

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -35,7 +35,54 @@ const Profile = () => {
                     <UserMenu/>
                 </div>
                 <div className="col-md-9">
-                    <h1>Your Profile</h1>
+                <div className="form-container">
+        <h4 className="title">REGISTER FORM</h4>
+        <form onSubmit={handleSubmit}>
+  <div className="mb-3">
+    
+    <input type="text" 
+    value={name}
+    onChange={(e)=>setName(e.target.value)}
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Name" 
+    required/>
+   
+  </div>
+  <div className="mb-3">
+    
+    <input type="email"
+    value={email}
+    onChange={(e)=>setEmail(e.target.value)}
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Email"  required />
+   
+  </div>
+  <div className="mb-3">
+   
+    <input type="password"
+    value={password} 
+    onChange={(e)=>setPassword(e.target.value)}
+    className="form-control" id="exampleInputPassword1" placeholder="Enter Your password" required />
+  </div>
+  <div className="mb-3">
+    
+    <input type="text" 
+    value={phone}
+    onChange={(e)=>setPhone(e.target.value)}
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Phone Number" required />
+   
+  </div>
+  <div className="mb-3">
+
+    <input type="text" 
+    value={address}
+    onChange={(e)=>setAddress(e.target.value)}
+    className="form-control" id="exampleInputEmail1" placeholder="Enter Your Address" required />
+   
+  </div>
+  
+  <button type="submit" className="btn btn-primary">UPDATE</button>
+</form>
+
+      </div>
                 </div>
             </div>
         </div>
