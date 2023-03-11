66710104737fa7abc1c6c45b04f5b227b338c300
capmahin <mahinulhuque1996@gmail.com>
2023-03-11 15:56:48 +0600

    condition is work

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -2,14 +2,25 @@ import React from 'react'
 import Layout from './../components/Layout/Layout';
 import { useCart } from "../context/cart";
 import { useAuth } from "../context/auth";
+import { useNavigate } from "react-router-dom";
 const CartPage = () => {
   const [auth, setAuth] = useAuth();
   const [cart, setCart] = useCart();
+  const navigate = useNavigate();
   return (
     <Layout>
         <div className="container">
             <div className="row">
-                <h1>Your cart</h1>
+              <div className="col-md-12">
+                <h1 className="text-center bg-light p-2 mb-1">
+                  {`Hellow ${auth?.token && auth?.user?.name}`}
+                </h1>
+                <h4 className="text-center">
+                  {cart?.length > 1 ? `You Have ${cart.length} items in your cart ${auth?.token ?
+                  "" : "please login to checkout"}
+                  ` : 'Your cart is empty'}
+                </h4>
+              </div>
             </div>
         </div>
     </Layout>
