a08b90a6527801949ea5d858f56f9e904037a4f3
capmahin <mahinulhuque1996@gmail.com>
2023-03-12 00:37:51 +0600

    import all state

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import UserMenu from "../../components/Layout/UserMenu";
 import Layout from './../../components/Layout/Layout';
+import { useAuth } from "../../context/auth";
 
 const Profile = () => {
+    //context
+    const [auth,setAuth] = useAuth()
+    //state
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phone, setPhone] = useState("")
+    const [address, setAddress] = useState("")
   return (
     <Layout title={'Your Profile'}>
         <div className="container-fluid m-3 p-3">
