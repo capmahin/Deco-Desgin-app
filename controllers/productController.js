1b9bcb4f7551f6706c08ab33d68e270accf384be
capmahin <mahinulhuque1996@gmail.com>
2023-03-13 03:55:53 +0600

    add dotenv.config()

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,12 @@
 import productModel from "../models/productModel.js"
 import categoryModel from "../models/categoryModel.js"
+import orderModel from "../models/orderModel.js";
 import fs from 'fs'
 import  slugify  from 'slugify';
 import braintree from 'braintree';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 //payment gateway
 var gateway = new braintree.BraintreeGateway({
@@ -11,7 +15,6 @@ var gateway = new braintree.BraintreeGateway({
     publicKey: process.env.BRAINTREE_PUBLIC_KEY,
     privateKey: process.env.BRAINTREE_PRIVATE_KEY,
   });
-
 export const createProductController = async (req,res)=>{
     try {
         const {name, slug, description,price,category,quantity,shipping} = req.fields
@@ -328,7 +331,30 @@ try {
 
 export const brainTreePaymentController = async (req,res) =>{
     try {
-        
+        const {cart, nonce} = req.body
+        let total = 0
+        cart.map( (i) => {total += i.price});
+         let newTransaction = gateway.transaction.sale({
+            amount:total,
+            paymentMethodNonce: nonce,
+            options:{
+               submitForSettlement:true 
+            }
+
+         },
+         function(error,result){
+            if(result){
+                const order = new orderModel({
+                    products :cart,
+                    payment: result,
+                    buyer: req.user._id
+                }).save()
+                res.json({ok:true})
+            }else{
+                res.status(500).send(error)
+            }
+         }
+         )
     } catch (error) {
         console.log(error)
     }
