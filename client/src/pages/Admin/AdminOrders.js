7365435d7f2d4732c7c015460aa219ac3c4b9872
capmahin <mahinulhuque1996@gmail.com>
2023-03-14 01:51:15 +0600

    add new page

---
+++ client/src/pages/Admin/AdminOrders.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AdminOrders = () => {
+  return (
+    <>
+    <div className="row">
+        <div className="col-md-3"></div>
+        <div className="col-md-9"></div>
+    </div>
+    </>
+  )
+}
+
+export default AdminOrders
